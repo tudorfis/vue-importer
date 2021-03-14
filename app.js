29059c6dc70828ff50faedab62b2cdc515f6538c
tudorfis <tudor.fis@gmail.com>
2021-03-14 06:58:18 +0200

    commit

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 import { importComponents } from './importer/importer.js'
 
-console.log(Vue)
-console.log(Vue.capitalize( 'asdada' ))
-
 Vue.createApp({
   template: '<app-component></app-component>',
   components: await importComponents([ 'app-component' ])
